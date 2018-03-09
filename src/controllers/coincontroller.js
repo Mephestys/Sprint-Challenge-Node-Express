41e8968d4d5a57d22f2de6ede77f1024c2b422a6
Mephestys <mephestys@gmail.com>
2018-03-09 11:53:03 -0700

    App receives data from endpoints, does not perform calculation of diff.

--- src/controllers/coincontroller.js
+++ src/controllers/coincontroller.js
@@ -4,13 +4,19 @@ const router = express.Router();
 const STATUS_SUCCESS = 200;
 const STATUS_USER_ERROR = 422;
 
-const {
-  getCurrentValue,
-  getPreviousValue,
-} = require('../models/compare.js');
+const { getValues } = require('../models/compare.js');
 
 router.get('/compare', (req, res) => {
-  console.log('Temp');
+  getValues()
+    .then(results => {
+      res.status(STATUS_SUCCESS)
+      res.send( {results: results })
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(STATUS_USER_ERROR)
+      res.send( {error: err })
+    });
 })
 
 module.exports = router;
--- src/models/compare.js
+++ src/models/compare.js
@@ -3,15 +3,23 @@ const fetch = require('node-fetch');
 const CURRENT_VALUE_ENDPOINT = 'https://api.coindesk.com/v1/bpi/currentprice/USD.json';
 const PREVIOUS_VALUE_ENDPOINT = 'https://api.coindesk.com/v1/bpi/historical/close.json?index=USD&for=yesterday';
 
-function getCurrentValue() {
+const endpoints = [CURRENT_VALUE_ENDPOINT, PREVIOUS_VALUE_ENDPOINT];
 
+function getValues() {
+  return new Promise((resolve, reject) => {
+    const valueArray = endpoints.map(endpoint => {
+      return fetch(endpoint)
+        .then(res => res.json())
+        .catch(err => {
+          console.log(err)
+        })
+    })
+    Promise.all(valueArray)
+      .then(valueArray => resolve(valueArray))
+      .catch(err => {
+        reject(err);
+      });
+  })
 }
 
-function getPreviousValue() {
-
-}
-
-module.exports = {
-  getCurrentValue,
-  getPreviousValue,
-}
+module.exports = { getValues }
